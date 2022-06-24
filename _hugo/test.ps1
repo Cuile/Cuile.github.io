22bfb0adf1e0bc2fa2d79bfae5676a5a9c14631a
Cuile <i@cuile.com>
2022-06-24 17:29:32 +0800

    Update test.ps1

--- _hugo/test.ps1
+++ _hugo/test.ps1
@@ -1,4 +1,4 @@
 # 测试过程中经常卡死，必须使用单独进程启动测试
-start .\hugo.exe "server --source ../ --theme=hugo-clarity --buildDrafts --minify --i18n-warnings --disableFastRender"
+start .\hugo.exe "server --source ../ --buildDrafts --minify --i18n-warnings --disableFastRender"
 # sleep 2
 # start microsoft-edge:http://localhost:1313
