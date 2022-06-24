9abf368d181945014531f2710563b1b3e5361cdb
Cuile <i@cuile.com>
2022-06-24 17:14:21 +0800

    Update test.ps1

--- _hugo/test.ps1
+++ _hugo/test.ps1
@@ -1,3 +1,4 @@
-.\hugo.exe server --source ../ --theme=clarity --buildDrafts --minify --i18n-warnings --disableFastRender
+# 测试过程中经常卡死，必须使用单独进程启动测试
+start .\hugo.exe "server --source ../ --theme=clarity --buildDrafts --minify --i18n-warnings --disableFastRender"
 # sleep 2
 # start microsoft-edge:http://localhost:1313
