f9d9c5aaad2ac174087689e5a1ff1088b91a923a
Cuile <i@cuile.com>
2022-06-24 20:32:05 +0800

    升级测试脚本，优化错误输出

--- _hugo/test.ps1
+++ _hugo/test.ps1
@@ -1,4 +1,14 @@
+# 清除上一次错误信息
+Clear-Content .\err.log
 # 测试过程中经常卡死，必须使用单独进程启动测试
-start .\hugo.exe "server --source ../ --buildDrafts --minify --i18n-warnings --disableFastRender"
-# sleep 2
+# 将错误信息输出到err.log中
+Start-Process .\hugo.exe "server --source ../ --theme=hugo-clarity --buildDrafts --minify --i18n-warnings --disableFastRender" -RedirectStandardError .\err.log
+Start-Sleep -Seconds 1
+# 读取err.log内容
+$err = Get-Content .\err.log
+# 如果有错误信息，则打印
+if ($err) {
+    Out-String -InputObject $err
+    # Out-String $err
+}
 # start microsoft-edge:http://localhost:1313
